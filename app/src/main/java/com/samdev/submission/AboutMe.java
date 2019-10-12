e4c0f1d2b627dccf03c08f5fa3822f036321f94a
SamX23 <skalexsong@gmail.com>
2019-10-12 16:18:18 +0700

    Add Floating button on About me and change the About Me layout into Androidx Constrain

--- app/src/main/java/com/samdev/submission/AboutMe.java
+++ app/src/main/java/com/samdev/submission/AboutMe.java
@@ -1,8 +1,12 @@
 package com.samdev.submission;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class AboutMe extends AppCompatActivity {
 
@@ -12,5 +16,14 @@ public class AboutMe extends AppCompatActivity {
         setContentView(R.layout.about_me);
         String title = " About Developer";
         setTitle(title);
+
+        FloatingActionButton floatbut = findViewById(R.id.floatbut);
+        floatbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Send Whatsapp to : 089657511134", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 }
